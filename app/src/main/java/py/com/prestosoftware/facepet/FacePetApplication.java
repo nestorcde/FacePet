cf943e5d4d6826e238c5269d2025388dbe989f59
nestorcde <nestorcde@gmail.com>
2018-11-17 16:58:28 -0300

    Commit Inicial

---
+++ app/src/main/java/py/com/prestosoftware/facepet/FacePetApplication.java
@@ -0,0 +1,13 @@
+package py.com.prestosoftware.facepet;
+
+import android.app.Application;
+
+public class FacePetApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //crashlitycs   -> Twitter CrashLitycs
+        //firebase Cloud Messaging -> FCM
+        //Basse de Datos Local -> Realm.io
+    }
+}
